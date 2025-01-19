3babe8d03ce90440ea9f3262012771a0742b0e5b
Shaiksufiyan157 <122155078+Shaiksufiyan157@users.noreply.github.com>
2025-01-20 00:15:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const panels=document.querySelectorAll('.panel')
+panels.forEach(panels=>{
+    panels.addEventListener('click',()=>{
+        removeActiveClasses()
+        panels.classList.add('active')
+    })
+})
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
+}
